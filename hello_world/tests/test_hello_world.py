5d77bf05fc3054cc2b9a7bf44a1353fca97b8b29
daryaVOLKK2 <darya.volk.volkova2001@gmail.com>
2024-10-10 21:36:43 +0000

    Update file test_hello_world.py

--- hello_world/tests/test_hello_world.py
+++ hello_world/tests/test_hello_world.py
@@ -1,38 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": None,
-   "id": "6fc0dba7-d8a8-4e4d-b9f8-e9254a7e0bb8",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from hello_world import main\n",
-    "\n",
-    "def test_main():\n",
-    "    assert main() is None\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+from hello_world import main
+
+def test_main():
+    assert main() is None
